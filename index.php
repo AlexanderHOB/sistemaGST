220061ab0a1d598bd317c6662664a75a04beafdc
Alexander Henry Obispo Buendia <obispoa1@gmail.com>
2019-03-01 15:53:40 -0500

    version beta-1

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Prueba</title>
-    <link rel="stylesheet" href="css/estilos">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Document</title>
+  <!-- HOJA DE ESTILOS-->
+  <link rel="stylesheet" href="https://oncebot.github.io/pushbar.js/pushbar/pushbar.css">
+
+  <link rel="stylesheet" href="css/estilos">
+  <!-- FAMILY FONT-->
+  <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+  <!--ICONS-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
 </head>
 <body>
     <?php 
         require_once "layouts/header.html";
         require_once "layouts/main.html";
         require_once "layouts/aside.html";
-        require_once "layouts/footer.html";
+        require_once "layouts/footer.php";
     ?>  
-    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="https://oncebot.github.io/pushbar.js/pushbar/pushbar.js"></script>
+
 	<script src="js/app.js"></script>
 </body>
 </html>
